743f5500a484aff599876c39e1e963a1268de2c0
siddhik92 <siddhikawade92@gmail.com>
2026-03-20 09:49:01 +0530

    api

--- teampulse/client/src/utils/api.js
+++ teampulse/client/src/utils/api.js
@@ -1,3 +1,33 @@
+// import axios from 'axios';
+
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL || '/api',
+//   headers: { 'Content-Type': 'application/json' },
+// });
+
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem('teampulse_token');
+//   if (token) config.headers.Authorization = `Bearer ${token}`;
+//   return config;
+// });
+
+// api.interceptors.response.use(
+//   (res) => res,
+//   (err) => {
+//     if (err.response?.status === 401) {
+//       localStorage.removeItem('teampulse_token');
+//       localStorage.removeItem('teampulse_user');
+//       window.location.href = '/login';
+//     }
+//     return Promise.reject(err);
+//   }
+// );
+
+// export default api;
+
+
+
+
 import axios from 'axios';
 
 const api = axios.create({
@@ -5,19 +35,21 @@ const api = axios.create({
   headers: { 'Content-Type': 'application/json' },
 });
 
+// Attach JWT token to every request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('teampulse_token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
 
+// Handle 401 globally
 api.interceptors.response.use(
   (res) => res,
   (err) => {
-    if (err.response?.status === 401) {
-      localStorage.removeItem('teampulse_token');
-      localStorage.removeItem('teampulse_user');
-      window.location.href = '/login';
+    if (err.response && err.response.status === 401) {
+      console.log("Unauthorized - maybe redirect to login");
     }
     return Promise.reject(err);
   }
